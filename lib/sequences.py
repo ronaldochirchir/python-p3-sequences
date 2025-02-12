b808b57713c03b3adc20a3d8cbd11e3dd836024f
ronaldochirchir <ronaldochirchir8@gmail.com>
2025-02-12 23:40:40 +0300

    completed assignments

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,11 @@
-#!/usr/bin/env python3
-
 def print_fibonacci(length):
-    pass
+    if length == 0:
+        print([])
+    elif length == 1:
+        print([0])
+    else:
+        fibonacci_sequence = [0, 1]
+        while len(fibonacci_sequence) < length:
+            next_number = fibonacci_sequence[-1] + fibonacci_sequence[-2]
+            fibonacci_sequence.append(next_number)
+        print(fibonacci_sequence)
